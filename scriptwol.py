d1458a4f59aea373d2cea3471b7895677c129033
CodingIsReal <163390606+codingisrealdev@users.noreply.github.com>
2024-03-14 10:11:50 +0100

    cambiando scriptwol.py
    cambiando el tipo de content-type

--- scriptwol.py
+++ scriptwol.py
@@ -7,7 +7,7 @@ import requests
 scriptwol= Flask(__name__)
 
 CORS(scriptwol)
-scriptwol.config["CORS_HEADERS"] = "Content type"
+scriptwol.config["CORS_HEADERS"] = "Content-type: text/html"
 
 
 url = "https://codingisrealdev.github.io/codingisrealdev/"
